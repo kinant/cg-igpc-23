980e29b1a29abccf6d3c2b3629c38418a06c4c60
Kinan T <kinan.t@gmail.com>
2023-02-15 19:20:16 -0600

    Input Validator
    Cleanup

--- InputValidator/InputValidator/UserInput.cpp
+++ InputValidator/InputValidator/UserInput.cpp
@@ -1,20 +1,3 @@
-#include "UserInput.h"
 
-template<class T>
-void InputValidator::GenericUserInput(T& input, std::string promptMessage)
-{
 
-	std::cout << promptMessage;
 
-	while (!(std::cin >> input)) 
-	{
-		ClearCin();
-		std::cout << "Invalid input. Try again: ";
-	}
-}
-
-void InputValidator::UserInput()
-{
-	int n;
-	GenericUserInput(n, "");
-}
--- InputValidator/InputValidator/UserInput.h
+++ InputValidator/InputValidator/UserInput.h
@@ -4,13 +4,20 @@
 
 #ifndef USER_INPUT_H
 #define USER_INPUT_H
+
+static const std::string ERROR_MESSAGE_GENERAL = "Invalid input. Try again:";
+
 class InputValidator
 {
 public:
 	
+
 	template<class T> static void GenericUserInput(T& input, std::string promptMessage);
+	
+	template<class T> static void GenericUserInput(T& input, std::string promptMessage, 
+		const int& min, const int& max);
+
 private:
-	static void UserInput();
 
 	static void ClearCin() 
 	{
@@ -18,4 +25,28 @@ private:
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 };
+
+template<class T>
+void InputValidator::GenericUserInput(T& input, std::string promptMessage)
+{
+	std::cout << promptMessage;
+
+	while (!(std::cin >> input))
+	{
+		ClearCin();
+		std::cout << ERROR_MESSAGE_GENERAL;
+	}
+}
+
+template<class T>
+inline void InputValidator::GenericUserInput(T& input, std::string promptMessage, const int& min, const int& max)
+{
+	std::cout << promptMessage;
+
+	while (!(std::cin >> input) || (input < min || input > max))
+	{
+		ClearCin();
+		std::cout << ERROR_MESSAGE_GENERAL;
+	}
+}
 #endif

3bf915ab1fdcbf663c9d0f9438972c9665e40fc1
Kinan T <kinan.t@gmail.com>
2023-02-17 23:42:08 -0600

    Editor
    Draws the tiles!!!

--- W06/W06/LevelEditor/LevelEditor.cpp
+++ W06/W06/LevelEditor/LevelEditor.cpp
@@ -17,6 +17,8 @@ void RenderBottomBorder(int width, int height);
 void RenderRightBorder(int height);
 void RenderLeftBorder(int width, int height);
 void RenderCursor(ACursor& Cursor);
+void UpdateCursorPosition(ACursor& Cursor, FLevel& Level);
+void HandleDrawInput(ACursor& Cursor, FLevel& Level);
 
 constexpr static int MAP_TILE_SIZE = 32;
 constexpr static int CURSOR_SIZE = 32;
@@ -62,7 +64,7 @@ int main()
 
 	InitWindow(screenWidth, screenHeight, "LEVEL EDITOR");
 
-	SetTargetFPS(15);
+	SetTargetFPS(10);
 
 	// Main game loop
 	while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -76,6 +78,8 @@ int main()
         RenderRightBorder(Level.Dimensions.Height + NUM_BORDER_ROWS);
         RenderLeftBorder(((Level.Dimensions.Width + NUM_BORDER_COLS) * MAP_TILE_SIZE) - MAP_TILE_SIZE, (Level.Dimensions.Height) + NUM_BORDER_ROWS);
         RenderCursor(Cursor);
+        UpdateCursorPosition(Cursor, Level);
+        HandleDrawInput(Cursor, Level);
         EndDrawing();
 	}
 
@@ -93,6 +97,9 @@ void GetLevelDimensions(FLevel& Level)
 
 void RenderLevel(FLevel& Level)
 {
+    int startX = (NUM_BORDER_COLS / 2);
+    int startY = (NUM_BORDER_ROWS / 2);
+
     for (unsigned int y = 0; y < Level.Level.size(); y++)
     {
         for (unsigned int x = 0; x < Level.Level[y].size(); x++)
@@ -103,28 +110,28 @@ void RenderLevel(FLevel& Level)
             case '|':
             case '+':
             {
-                DrawRectangle((x + 1) * MAP_TILE_SIZE, (y + 1) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CWalls);
+                DrawRectangle((x + startX) * MAP_TILE_SIZE, (y + startY) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CWalls);
                 break;
             }
             case 'D':
             {
-                DrawRectangle(x * MAP_TILE_SIZE, y * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CDoor);
+                DrawRectangle((x + startX) * MAP_TILE_SIZE, (y + startY) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CDoor);
                 break;
             }
             case 'X':
             {
-                DrawRectangle(x * MAP_TILE_SIZE, y * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CGoal);
+                DrawRectangle((x + startX) * MAP_TILE_SIZE, (y + startY) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CGoal);
                 break;
             }
             case '*':
             {
-                DrawRectangle(x * MAP_TILE_SIZE, y * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CKey);
+                DrawRectangle((x + startX) * MAP_TILE_SIZE, (y + startY) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, CKey);
                 break;
             }
             default:
                 break;
             }
-            DrawRectangleLines((x + 1) * MAP_TILE_SIZE, (y + 1) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, Fade(CWalls, 0.5f));
+            DrawRectangleLines((x + startX) * MAP_TILE_SIZE, (y + startY) * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, Fade(CWalls, 0.5f));
         }
     }
 }
@@ -163,10 +170,97 @@ void RenderLeftBorder(int width, int height)
 
 void RenderCursor(ACursor& Cursor) 
 {
-    int startX = (1 + NUM_BORDER_COLS / 2) * MAP_TILE_SIZE;
-    int startY = (1 + NUM_BORDER_ROWS / 2) * MAP_TILE_SIZE;
+    int startX = (NUM_BORDER_COLS / 2);
+    int startY = (NUM_BORDER_ROWS / 2);
 
-    DrawRectangle((Cursor.GetPosition().X + startX), (Cursor.GetPosition().Y + startY), CURSOR_SIZE, CURSOR_SIZE, YELLOW);
+    DrawRectangle((Cursor.GetPosition().X + startX) * MAP_TILE_SIZE, (Cursor.GetPosition().Y + startY) * MAP_TILE_SIZE, CURSOR_SIZE, CURSOR_SIZE, YELLOW);
 
 
+}
+
+void UpdateCursorPosition(ACursor& Cursor, FLevel& Level) 
+{
+    FCoordinates NewPosition{ Cursor.GetPosition() };
+
+    if (IsKeyDown(KEY_W))
+    {
+        NewPosition.Y--;
+    }
+    else if (IsKeyDown(KEY_S))
+    {
+        NewPosition.Y++;
+    }
+    else if (IsKeyDown(KEY_A))
+    {
+        NewPosition.X--;
+    }
+    else if (IsKeyDown(KEY_D))
+    {
+        NewPosition.X++;
+    }
+    
+    int boundsLeft = (NUM_BORDER_ROWS / 2) * MAP_TILE_SIZE;
+    int boundsRight = ((Level.Dimensions.Width) * MAP_TILE_SIZE) - (1 + NUM_BORDER_COLS / 2) * MAP_TILE_SIZE;
+    int boundsTop = (NUM_BORDER_ROWS / 2) * MAP_TILE_SIZE;
+    int boundsBottom = ((Level.Dimensions.Height) * MAP_TILE_SIZE) - (1 + NUM_BORDER_ROWS / 2) * MAP_TILE_SIZE;
+
+    cout << endl;
+
+    cout << "LEFT BOUND: " << boundsLeft << endl;
+    cout << "RIGHT BOUND: " << boundsRight << endl;
+    cout << "TOP BOUND: " << boundsTop << endl;
+    cout << "BOTTOM BOUND: " << boundsBottom << endl;
+
+    cout << endl;
+
+    cout << "NEW X: " << NewPosition.X * MAP_TILE_SIZE << endl;
+    cout << "NEW Y: " << NewPosition.Y * MAP_TILE_SIZE << endl;
+
+    if ((NewPosition.X * MAP_TILE_SIZE >= boundsLeft && NewPosition.X * MAP_TILE_SIZE <= boundsRight) 
+        && (NewPosition.Y * MAP_TILE_SIZE <= boundsBottom && NewPosition.Y * MAP_TILE_SIZE >= boundsTop)) 
+    {
+        Cursor.SetPosition(NewPosition);
+    }
+
+}
+
+void HandleDrawInput(ACursor& Cursor, FLevel& Level)
+{
+    char& MapTile = Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X];
+
+    if (IsKeyReleased(KEY_H)) 
+    {
+        if (MapTile == (char)Map::EMapTile::Empty) 
+        {
+            Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X] = (char)Map::EMapTile::HWall;
+        }
+    }
+    if (IsKeyReleased(KEY_J))
+    {
+        if (MapTile == (char)Map::EMapTile::Empty)
+        {
+            Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X] = (char)Map::EMapTile::Door;
+        }
+    }
+    if (IsKeyReleased(KEY_K))
+    {
+        if (MapTile == (char)Map::EMapTile::Empty)
+        {
+            Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X] = (char)Map::EMapTile::Key;
+        }
+    }
+    if (IsKeyReleased(KEY_G))
+    {
+        if (MapTile == (char)Map::EMapTile::Empty)
+        {
+            Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X] = (char)Map::EMapTile::Goal;
+        }
+    }
+    if (IsKeyReleased(KEY_DELETE))
+    {
+        if (MapTile != (char)Map::EMapTile::Empty)
+        {
+            Level.Level[Cursor.GetPosition().Y][Cursor.GetPosition().X] = (char)Map::EMapTile::Empty;
+        }
+    }
 }

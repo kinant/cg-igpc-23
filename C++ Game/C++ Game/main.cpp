cf132f451c3fbda8e2643148d1a9720a57cc4ce8
Kinan T <kinan.t@gmail.com>
2023-02-23 18:50:55 -0600

    M07C08P04

--- C++ Game/C++ Game/main.cpp
+++ C++ Game/C++ Game/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
 using std::cout;
 using std::endl;
 
 constexpr char kPlayerSymbol = '@';
 
+constexpr char WAL = 219;
+constexpr char KEY = 232;
+constexpr char DOR = 179;
+constexpr char GOL = 36;
+
+constexpr int kOpenDoorColor = 10;
+constexpr int kClosedDoorColor = 12;
+constexpr int kRegularColor = 7;
+
 int GetIndexFromCoordinates(const int X, const int Y, const int Width);
-void DrawLevel(const char Level[], const int Width, const int Height, const int PlayerX, const int PlayerY);
+void DrawLevel(const char Level[], const int Width, const int Height, const int PlayerX, const int PlayerY, const bool PlayerHasKey);
 bool UpdatePlayerPosition(char Level[], const int Width, int& PlayerX, int& PlayerY, bool& PlayerHasKey);
+
+void PlayDoorClosedSound();
+void PlayDoorOpenSound();
+void PlayKeyPickupSound();
+void PlayWinSound();
 
 int main() 
 {
@@ -16,21 +31,21 @@ int main()
     constexpr int kHeight = 15;
 
 	char LevelArray[] = {
-            '+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ','*','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','-',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','-','-','-','-','-','-','-','-','-','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-            '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','D',' ',' ',' ',' ',' ',' ',' ',' ','X','|',
-            '+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+'
+            WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',KEY,WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,WAL,' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+            WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',DOR,' ',' ',' ',' ',' ',' ',' ',' ',GOL,WAL,
+            WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL
     };
 
     int PlayerX = 1;
@@ -42,13 +57,14 @@ int main()
     while (!GameOver) 
     {
         system("CLS");
-        DrawLevel(LevelArray, kWidth, kHeight, PlayerX, PlayerY);
+        DrawLevel(LevelArray, kWidth, kHeight, PlayerX, PlayerY, PlayerHasKey);
         GameOver = UpdatePlayerPosition(LevelArray, kWidth, PlayerX, PlayerY, PlayerHasKey);
     }
 
     system("CLS");
-    DrawLevel(LevelArray, kWidth, kHeight, PlayerX, PlayerY);
+    DrawLevel(LevelArray, kWidth, kHeight, PlayerX, PlayerY, PlayerHasKey);
     cout << "YOU WON!!!";
+    PlayWinSound();
 
 }
 
@@ -57,7 +73,7 @@ int GetIndexFromCoordinates(int X, int Y, int Width)
     return X + Y * Width;
 }
 
-void DrawLevel(const char Level[], const int Width, const int Height, const int PlayerX, const int PlayerY)
+void DrawLevel(const char Level[], const int Width, const int Height, const int PlayerX, const int PlayerY, const bool PlayerHasKey)
 {
     for (int Y = 0; Y < Height; Y++) 
     {
@@ -70,6 +86,24 @@ void DrawLevel(const char Level[], const int Width, const int Height, const int 
             }
 
             const int Index = GetIndexFromCoordinates(X, Y, Width);
+            HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            if (Level[Index] == DOR) 
+            {
+                if (PlayerHasKey) 
+                {
+                    SetConsoleTextAttribute(console, kOpenDoorColor);
+                }
+                else 
+                {
+                    SetConsoleTextAttribute(console, kClosedDoorColor);
+                }
+            }
+            else 
+            {
+                SetConsoleTextAttribute(console, kRegularColor);
+            }
+
             cout << Level[Index];
         }
         cout << endl;
@@ -120,21 +154,27 @@ bool UpdatePlayerPosition(char Level[], const int Width, int& PlayerX, int& Play
         PlayerX = NewPlayerX;
         PlayerY = NewPlayerY;
     }
-    else if (Level[Index] == '*') 
+    else if (Level[Index] == KEY) 
     {
         PlayerHasKey = true;
         Level[Index] = ' ';
         PlayerX = NewPlayerX;
         PlayerY = NewPlayerY;
+        PlayKeyPickupSound();
     }
-    else if (Level[Index] == 'D' && PlayerHasKey) 
+    else if (Level[Index] == DOR && PlayerHasKey) 
     {
         PlayerHasKey = false;
         Level[Index] = ' ';
         PlayerX = NewPlayerX;
         PlayerY = NewPlayerY;
+        PlayDoorOpenSound();
     }
-    else if (Level[Index] == 'X') 
+    else if (Level[Index] == DOR && !PlayerHasKey) 
+    {
+        PlayDoorClosedSound();
+    }
+    else if (Level[Index] == GOL) 
     {
         Level[Index] = ' ';
         PlayerX = NewPlayerX;
@@ -143,4 +183,33 @@ bool UpdatePlayerPosition(char Level[], const int Width, int& PlayerX, int& Play
     }
 
     return false;
+}
+
+void PlayDoorClosedSound() 
+{
+    Beep(500, 75);
+    Beep(500, 75);
+
+}
+
+void PlayDoorOpenSound() 
+{
+    Beep(1397, 200);
+}
+
+void PlayKeyPickupSound() 
+{
+    Beep(1568, 200);
+}
+
+void PlayWinSound() 
+{
+    Beep(1568, 200);
+    Beep(1568, 200);
+    Beep(1568, 200);
+    Beep(1245, 1000);
+    Beep(1397, 200);
+    Beep(1397, 200);
+    Beep(1397, 200);
+    Beep(1175, 1000);
 }

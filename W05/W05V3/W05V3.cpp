f6cf2b4bf727d5cc538232548e5c0fbe4a6b4ac9
Kinan T <kinan.t@gmail.com>
2023-03-01 16:42:36 -0600

    W05V3 -  Working condition
    Commented

--- W05/W05V3/W05V3.cpp
+++ W05/W05V3/W05V3.cpp
@@ -9,6 +9,29 @@ using std::string;
 using std::cout;
 using std::endl;
 
+// uncomment, and comment the other one, to test extreme cases
+//const char* kDigits = "\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000005555500000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000009979900000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000000000000000000000000\
+//00000000000000000000000000000099999000000000000000\
+//00000000000000000000000000000000000000000000000000";
+
 const char* kDigits = "\
 73167176531330624919225119674426574742355349194934\
 96983520312774506326239578318016984801869478851843\
@@ -55,22 +78,34 @@ int main()
 			break;
 		}
 
+		// get the first digit of the "previous" iteration
 		int FirstDigit = CharToInt(kDigits[i]);
+
+		// get the nexy digit of the next iteration
 		int NextDigit = CharToInt(kDigits[i + SEQ_LEN]);
 
-		// if we hit a first digit with 0, we skip to the next iteration...
+		// if we hit a first digit with 0, we skip to the next iteration
 		if (FirstDigit == 0) 
 		{
 			GetNextValidIteration(i, Product, Length);
+			
+			// we (possibliy) incremented i in function, the index returned hast to be 
+			// subtracted by 1, since the loop condition will add one again. 
 			i--;
 
 			if (Max < Product) 
 			{
 				Max = Product;
 			}
+			// continue on to next iteration skipping as many iterations as given by the new i value
 			continue;
 		}
+		// iteration not skipped
+
+		// divide the product by the first digit of the "previous" iteration
 		Product /= FirstDigit;
+
+		// multiply by the next digit of the next iteration to get the new product
 		Product *= NextDigit;
 
 		if (Max < Product) 
@@ -82,33 +117,61 @@ int main()
 	cout << "MAX PRODUCT OF " << SEQ_LEN <<" CONSECUTIVE DIGITS : " << Max << endl;
 }
 
+/*
+* Function - GetNextValidIteration
+* Loops through a number of digits, until a valid sequence is found.
+* A valid sequence is defined as one in which all its N digits, given by SEQ_LEN,
+* can be multiplied without resulting in a total product.
+* 
+* @param -	ValidIterationIndex: Reference to the index that will be used to start searching.
+*			Eventually it get's overriten to the index of the first digit valid iteration.
+*
+* @param -	Product: reference to the product. Rather than return a product in this function, I
+*			decided to just pass a reference. Once the function is complete, the Product should be
+*			set to the valid iteration Product, or to 0 if none found. 
+* 
+* @param - Length: just a reference to the length of the array of digits
+*/
+
 void GetNextValidIteration(int& ValidIterationIndex, int& Product, const int& Length)
 {
+	// flag for checking if a valid input found
 	bool bValidIterationFound = false;
+	
+	// we will check that SEQ_LEN digits were multiplied in order to make the above true
 	int DigitsMultiplied = 0;
 
-	// Get the 1st valid iteration
+	// loop until we get a valid iteration
 	while (!bValidIterationFound)
 	{
+		// reset
 		DigitsMultiplied = 0;
 		Product = 1;
 
+		// if we have reached the the max number of digits in the array, or above, we break out.
 		if (SEQ_LEN + ValidIterationIndex >= Length)
 		{
 			Product = 0;
+			break;
 		}
+
+		// iterate over the sequence of digits
 		for (int i = ValidIterationIndex; i < SEQ_LEN + ValidIterationIndex; i++)
 		{
 			int Digit = CharToInt(kDigits[i]);
 
+			// if any digit in the sequence is 0, then no valid iteration is found, we skip to the next
 			if (Digit == 0)
 			{
 				ValidIterationIndex++;
 				break;
 			}
+			 
 			Product *= Digit;
 			DigitsMultiplied++;
 		}
+
+		// if we multiplied SEQ_LEN number of digits, then we did not have a single 0 in the sequence, valid iteration sequence found!
 		if (DigitsMultiplied == SEQ_LEN)
 		{
 			bValidIterationFound = true;
